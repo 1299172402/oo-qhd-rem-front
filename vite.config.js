0b115f7b7a3cf20d21a5d19ec8f08c32e5a756bd
ex_liuhuan2 <97726dd1982d4ed7a6cdb6ab6c5296ec@example.com>
2023-01-18 15:31:43 +0800

    更新vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -72,7 +72,7 @@ export default ({ mode }) => {
         '/dev-api': {
           // 用于开发环境下的转发请求
           // 更多请参考：https://vitejs.dev/config/#server-proxy
-          target: 'http://192.168.1.108:31585',
+          target: 'http://localhost:80/dev-api/',
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/dev-api/, ''),
         },
