474dca462b7d3cbca2ffaf163401b8cf58f67235
ex_liuhuan2 <97726dd1982d4ed7a6cdb6ab6c5296ec@example.com>
2023-02-09 17:36:49 +0800

    更新host.ts

--- src/config/host.ts
+++ src/config/host.ts
@@ -27,7 +27,7 @@ export default {
     // 文件预览服务
     PREVIEW_FILE_API: "文件服务地址",
     // 正式环境接口地址
-    API: 'http://10.77.78.221:31995/prod-api/',
+    API: 'http://10.77.79.177:31995/prod-api/',
     // 正式环境 cdn 路径
     CDN: '',
   },
