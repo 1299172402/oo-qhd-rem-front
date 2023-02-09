3c53902f415278f7c8b19db8a1a85c68d0159e5b
guantangzheng <2804257836@qq.com>
2023-02-09 17:00:47 +0800

    feat:存放部署文件

--- src/pages/common/api/status.ts
+++
@@ -1,7 +0,0 @@
-import { getAction } from "@/components/audit/process/api/manage";
-
-export function getProcessStatus() {
-  return getAction("/sys/dict/getDictItems/process_instance_status")
-}
-
-export default {};
