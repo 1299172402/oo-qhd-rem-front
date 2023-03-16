f4318192a7ff454b6be3ae4bc788db67efcfe94b
张西澎 <2395873343@qq.com>
2023-03-16 15:42:12 +0800

    修改路由文件

--- src/router/modules/base.ts
+++ src/router/modules/base.ts
@@ -3,36 +3,36 @@ import Layout from '@/layouts/index.vue';
 
 export default [
     // 三级菜单写法示例
-    {
-        path: '/menu',
-        name: 'menu',
-        component: Layout,
-        hidden: false,
-        meta: { title: '一级菜单', icon: 'menu-fold' },
-        children: [
-            {
-                path: 'second',
-                meta: { title: '二级菜单-1' },
-                component: () => import('@/layouts/blank.vue'), // 必须是这个页面
-                children: [
-                    {
-                        hidden: false,
-                        path: 'aa',
-                        name: 'aa',
-                        component: () => import('@/pages/examplePage/nestMenu/index.vue'),
-                        meta: { title: '三级菜单-1-1', icon: 'icon' }, // 三级必加icon,为了缩进
-                    },
-                    {
-                        hidden: false,
-                        path: 'bb',
-                        name: 'bb',
-                        component: () => import('@/pages/examplePage/dashboard/detail/index.vue'),
-                        meta: { title: '三级菜单-1-2', icon: 'icon' },
-                    },
-                ],
-            },
-        ],
-    },
+    // {
+    //     path: '/menu',
+    //     name: 'menu',
+    //     component: Layout,
+    //     hidden: false,
+    //     meta: { title: '一级菜单', icon: 'menu-fold' },
+    //     children: [
+    //         {
+    //             path: 'second',
+    //             meta: { title: '二级菜单-1' },
+    //             component: () => import('@/layouts/blank.vue'), // 必须是这个页面
+    //             children: [
+    //                 {
+    //                     hidden: false,
+    //                     path: 'aa',
+    //                     name: 'aa',
+    //                     component: () => import('@/pages/examplePage/nestMenu/index.vue'),
+    //                     meta: { title: '三级菜单-1-1', icon: 'icon' }, // 三级必加icon,为了缩进
+    //                 },
+    //                 {
+    //                     hidden: false,
+    //                     path: 'bb',
+    //                     name: 'bb',
+    //                     component: () => import('@/pages/examplePage/dashboard/detail/index.vue'),
+    //                     meta: { title: '三级菜单-1-2', icon: 'icon' },
+    //                 },
+    //             ],
+    //         },
+    //     ],
+    // },
     //   {
     //     path: '/dashboard',
     //     component: Layout,
