ead1d5185fd1c121493ae89fcc755b23bba91785
张西澎 <2395873343@qq.com>
2023-03-22 08:57:09 +0800

    上传迁移模块

---
+++ src/router/modules/injection.ts
@@ -0,0 +1,20 @@
+import Layout from '@/layouts/index.vue';
+
+export default [
+  {
+    path: '/injection',
+    name: 'injection',
+    hidden: false,
+    component: Layout,
+    meta: { title: '智能注采调配', icon: 'client', single: false },
+    children: [
+      {
+        path: 'injection',
+        name: 'injection',
+        component: () => import('@/pages/rem/injection/yieldSplitting.vue'),
+        meta: { title: '产量劈分',icon: 'icon' },
+      },
+    ],
+    
+  },
+];
