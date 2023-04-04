fefe39e23296ef9b750e6256517993c7110dbf13
Dear_Zxb <13521265431@163.com>
2023-04-04 10:46:17 +0800

    放开本地路由

--- src/router/modules/dynamicManagement.ts
+++ src/router/modules/dynamicManagement.ts
@@ -2,7 +2,7 @@ import Layout from '@/layouts/index.vue';
 
 export default [
     {
-        hidden: true,
+        hidden: false,
         path: '/dynamicManagement',
         name: 'dynamicManagement',
         component: Layout,
