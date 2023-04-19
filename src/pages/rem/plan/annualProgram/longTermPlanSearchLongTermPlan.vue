4c8318dea4043e3167b0f4f9f137f50df227559a
CNOOC/qianlq2 <qingqing1990@A>
2023-04-19 08:54:20 +0800

    中长期开发计划

--- src/pages/rem/plan/annualProgram/longTermPlanSearchLongTermPlan.vue
+++ src/pages/rem/plan/annualProgram/longTermPlanSearchLongTermPlan.vue
@@ -19,7 +19,7 @@
     </header-search>
     <page-panel-new class="tablebox" :show-btn="true">
       <div class="tableTitle">{{ oilFieldName }} 中长期计划表</div>
-      <el-table :data="tableData" id="tableData" height="calc(100% - 40px)" highlight style="width: 100%">
+      <el-table :data="tableData2" id="tableData" height="calc(100% - 40px)" highlight style="width: 100%">
         <el-table-column prop="theYear" align="center" label="年份"></el-table-column>
         <el-table-column prop="baseProduct" align="center" :label="`基础产量\n(10⁴m³)`"></el-table-column>
         <el-table-column prop="newJustWellNum" align="center" :label="`新增调整井井数\n(口)`"></el-table-column>
