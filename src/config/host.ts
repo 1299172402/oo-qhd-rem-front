61122d713a9884e9e50ed9c909429eb8ec41c343
张西澎 <2395873343@qq.com>
2023-05-05 17:23:00 +0800

    修改host服务

--- src/config/host.ts
+++ src/config/host.ts
@@ -61,9 +61,10 @@ export default {
     // 文件预览服务
     PREVIEW_FILE_API: "文件服务地址",
     // 正式环境接口地址
-    API: "http://10.77.78.250:80/prod-api/",
+    API: 'https://rem.tjioms-dev.tjltd.cnooc/prod-api/',
     // 正式环境 cdn 路径
-    CDN: "",
+    CDN: '',
+    appId: 'f198c1a239254b0e86529a0668cf4adb',
     LOGIN_TYPES: ["corp_oauth", "password"]
   }
 };
