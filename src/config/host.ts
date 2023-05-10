f8b8a1dcf5e1a330e3da96fbe6a7ad3c3938e6e6
zhangjing <2237612155@qq.com>
2023-05-10 17:19:31 +0800

    修改

--- src/config/host.ts
+++ src/config/host.ts
@@ -11,7 +11,7 @@ const config = {
   appId: "$system$",
   ALARM_URL: "https://amm.tjioms-dev.tjltd.cnooc/#/reportPolice/source?access_token=",
   WEB_TAG_NAME: "智能油田管理系统", // 网页标签名称
-  SYSTEM_NAME: "智能油田管理系统" // 系统名称
+  SYSTEM_NAME: "天津分公司智能油田管理系统" // 系统名称
 };
 
 export default {
