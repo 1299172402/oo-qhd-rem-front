3b28290058175aba22f255d3f870f01e6464ce42
张西澎 <2395873343@qq.com>
2023-05-17 14:47:40 +0800

    看板API

--- src/api/rem/reservoirbillboards.js
+++ src/api/rem/reservoirbillboards.js
@@ -1,24 +1,27 @@
 import request from '@/utils/request'
-
 const baseUrl = process.env.NODE_ENV == "production" ? "/rem/api" : "/rem/api"
 
-export function getYieldFluctuation({date, dateComp, ogfId} = {}) {
-    return request({
-        url: `${baseUrl}/reservoirBillboards/getYieldFluctuation?date=${date || ""}&dateComp=${dateComp || ""}&ogfId=${ogfId || ""}`,
-        method: "get",
-    });
+export function getYieldFluctuation({ date, dateComp, ogfId } = {}) {
+  return request({
+    url: `${baseUrl}/reservoirBillboards/getYieldFluctuation?date=${ date || "" }&dateComp=${ dateComp || "" }&ogfId=${ ogfId || "" }`,
+    method: "get",
+  });
 }
-
-export function productionMetricsOverview({date} = {}) {
-    return request({
-        url: `${baseUrl}/reservoirBillboards/productionMetricsOverview?date=${date || ""}`,
-        method: "get",
-    });
+export function productionMetricsOverview({ date } = {}) {
+  return request({
+    url: `${baseUrl}/reservoirBillboards/productionMetricsOverview?date=${ date || "" }`,
+    method: "get",
+  });
 }
-
-export function queryYieldTracking({ogfId, orgId} = {}) {
-    return request({
-        url: `${baseUrl}/reservoirBillboards/queryYieldTracking?ogfId=${ogfId || ""}&orgId=${orgId || ""}`,
-        method: "get",
-    });
+export function queryProblemWellStatis({ date, platformId, wellId } = {}) {
+  return request({
+    url: `${baseUrl}/reservoirBillboards/queryProblemWellStatis?date=${ date || "" }&platformId=${ platformId || "" }&wellId=${ wellId || "" }`,
+    method: "get",
+  });
+}
+export function queryYieldTracking({ ogfId, orgId } = {}) {
+  return request({
+    url: `${baseUrl}/reservoirBillboards/queryYieldTracking?ogfId=${ ogfId || "" }&orgId=${ orgId || "" }`,
+    method: "get",
+  });
 }
