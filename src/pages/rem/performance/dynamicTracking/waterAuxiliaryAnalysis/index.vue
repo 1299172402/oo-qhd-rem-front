101fafda77888452e386f0b47779253d06d62da7
董鲁军 <t-lujun.dong@pcitc.com>
2023-06-13 14:41:05 +0800

    分层调配及视吸水指数跳转更换/示踪剂页面隐藏上传文档按钮-D

--- src/pages/rem/performance/dynamicTracking/waterAuxiliaryAnalysis/index.vue
+++ src/pages/rem/performance/dynamicTracking/waterAuxiliaryAnalysis/index.vue
@@ -524,7 +524,7 @@
                     window.open(url,'_blank');
                 }
                 else if (module.name == "injectivityIndex") {
-                    window.open("https://ipm.tjioms-dev.tjltd.cnooc/#/waterflood/waterRunningControl?link=rem", "_blank");
+                    window.open("https://ipm.tjioms-dev.tjltd.cnooc/#/waterflood/merge", "_blank");
                 }else{
                     this.currentModule = module.name;
                 }
