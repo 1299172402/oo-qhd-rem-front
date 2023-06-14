14893fb1834e614dedca661a5b0333b79b4fca55
张西澎 <2395873343@qq.com>
2023-06-14 10:11:32 +0800

    超时时间

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -65,7 +65,7 @@ function hideLoading() {
 
 const instance = axios.create({
   baseURL: API_HOST,
-  timeout: 10000,
+  timeout: 30000,
   withCredentials: true,
   headers: {
     "App-Id": proxy[env].appId
