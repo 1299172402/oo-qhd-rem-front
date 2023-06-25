adf786d076c91b0956d84071db1cf0349c8b5942
董鲁军 <t-lujun.dong@pcitc.com>
2023-06-25 18:43:17 +0800

    注采联动添加禁止点击 -D

--- src/pages/rem/home/linkage/linkageBox/index.vue
+++ src/pages/rem/home/linkage/linkageBox/index.vue
@@ -26,7 +26,8 @@
                     <div class="pad">
                         <div>
                             <p v-show="!content" :key="index" v-for="(item,index) in currentList.boxBottomText">
-                                <span style="cursor: pointer" @click="linkTo(item.url)">{{ item.name ? item.name : item }}</span>
+                                <span v-if="item.url" style="cursor: pointer" @click="linkTo(item.url)">{{ item.name ? item.name : item }}</span>
+                                <span v-if="!item.url" style="pointer-events: none;color:#5a5959;font-weight:bolder">{{ item.name ? item.name : item }}</span>
                                 <span v-if="currentList.boxBottomContent" class="btnContent" @click="btnContent(index)">{{ currentList.boxBottomContent[index].length > 0 ? '>>' : '' }}</span>
                                 <span v-else class="btnBack" @click="btnBack"></span>
                             </p>
