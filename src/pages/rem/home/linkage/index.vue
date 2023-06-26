93604fef753d180cb8446117cef806fcfc144d56
董鲁军 <t-lujun.dong@pcitc.com>
2023-06-26 14:25:44 +0800

    跳转参数 -D

--- src/pages/rem/home/linkage/index.vue
+++ src/pages/rem/home/linkage/index.vue
@@ -144,8 +144,8 @@ export default {
                     boxText: '注采状况分析',
                     boxBottomText: [
                         {name:'注采连通分析',url:'https://rem.tjioms-dev.tjltd.cnooc/#/basic/wellGroup_Maintenance?link=linkage'},
-                        {name:'注采平衡分析',url:'https://rem.tjioms-dev.tjltd.cnooc/#/dynamicManagement/dynamicTrackingBlock/blockAnalysisReport'},
-                        {name:'采出状况分析',url:'https://rem.tjioms-dev.tjltd.cnooc/#/dynamicManagement/dynamicTrackingBlock/blockAnalysisReport'}
+                        {name:'注采平衡分析',url:'https://rem.tjioms-dev.tjltd.cnooc/#/dynamicManagement/dynamicTrackingBlock/blockAnalysisReport?link=1'},
+                        {name:'采出状况分析',url:'https://rem.tjioms-dev.tjltd.cnooc/#/dynamicManagement/dynamicTrackingBlock/blockAnalysisReport?link=2'}
                     ],
                     boxBottomContent: [
                         [{name:'注采连通性评价模型',url:''}],
