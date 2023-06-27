0309eea17daaea231365857cf1f6d9cda02566ff
CNOOC/ex_dingshh <13521265431@163.com>
2023-06-27 18:06:44 +0800

    zxb

--- src/layouts/blank.vue
+++ src/layouts/blank.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="myWrapper">
-    <keep-alive>
-      <router-view />
-    </keep-alive>
+      <router-view/>
   </div>
 </template>
+<script>
+    export default{
+        name:'blank',
+    }
+</script>
 <style lang="less" scoped>
 .myWrapper {
   height: 100%;
