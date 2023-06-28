41c005f7c075e1e0a062846506902313e9af0059
张西澎 <2395873343@qq.com>
2023-06-28 21:45:18 +0800

    修改页面

--- src/pages/rem/intelligence/optimization/index.vue
+++ src/pages/rem/intelligence/optimization/index.vue
@@ -7,6 +7,7 @@
                     <!-- <a href="http://sea-oil-web-qhd32-6znyt.tjdevapp.cnooc/"></a> -->
                 </el-tab-pane>
             </el-tabs>
+            <el-button v-if ="activeName =='second' && this.$route.query.link " style="position: absolute;right: 20px;top:110px" type="primary" @click="gogo">返回</el-button>
         </div>
         <header-search  style="height: auto;display: grid">
             <div v-if="activeName == 'first'" style="margin-top:20px;margin-bottom:20px;">
@@ -503,6 +504,9 @@ export default {
             this.iframeWidth = this.$refs.iframe.parentNode.clientWidth;
         }
     },
+    gogo() {
+        window.open('https://rem.tjioms-dev.tjltd.cnooc/#/reservoirDisplay/linkage', "_parent");
+    },
     mounted() {
         this.$nextTick(()=>{
             setTimeout(()=>{
@@ -516,6 +520,7 @@ export default {
             this.activeName = 'second'
             this.doSearch()
         }else{
+            this.activeName = 'first'
             this.doSearch()
         }
     },
