1b1ce51750bc6ecdaab160d1ea46525604b0c605
董鲁军 <t-lujun.dong@pcitc.com>
2023-06-28 10:18:09 +0800

    修改归因wellName -D

--- src/pages/rem/performance/dynamicTracking/oilAuxiliaryAnalysis/attributtonAnalysis.vue
+++ src/pages/rem/performance/dynamicTracking/oilAuxiliaryAnalysis/attributtonAnalysis.vue
@@ -34,7 +34,7 @@
                         </el-form-item>
                         <el-form-item label="井号:">
                             <el-select v-model="queryData.well" clearable style="width: 170px">
-                                <el-option v-for="(item, index) in wellList" :key="index" :label="item.wellNo"
+                                <el-option v-for="(item, index) in wellList" :key="index" :label="item.wellName"
                                            :value="item.wellId">
                                 </el-option>
                             </el-select>
