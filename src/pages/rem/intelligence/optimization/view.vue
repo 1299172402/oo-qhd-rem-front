db475bd5560188de150ca0f1836e989f6e67561d
张西澎 <2395873343@qq.com>
2023-07-01 10:06:40 +0800

    修改页面

--- src/pages/rem/intelligence/optimization/view.vue
+++ src/pages/rem/intelligence/optimization/view.vue
@@ -505,7 +505,6 @@ export default {
                     item.wellIndex = wellIndex.toString();
                     item.layerIndex = layerIndex.toString();
                 })
-                console.log(arr)
                 this.form.tableData2 = arr
                 // this.getSpanArr(arr, 'injWellNo')
                 this.groupBy(arr)
