c64eba041b42a7256550636a76dc23efc0a8d1c4
CNOOC/ex_dingshh <13521265431@163.com>
2023-07-04 19:32:10 +0800

    zxb

--- src/pages/rem/plan/annualPlan/crudeOil/index.vue
+++ src/pages/rem/plan/annualPlan/crudeOil/index.vue
@@ -40,8 +40,8 @@
     import dayjs from "dayjs";
     import Echart from '@/components/tools/Echarts/index.vue';
     import { searchOilProductionChart, searchOilProductionTable} from '@/api/oilDeposit/rem-03/oilfieldmanageplan.js';
+    import { downLoadOilProductionTable} from '@/api/oilDeposit/rem-04/plan.js';
     import { exportExcel } from '@/lib/exportExcel.js';
-    
     import FileSaver from 'file-saver';
     import * as XLSX from '@/lib/xlsx'
     
@@ -401,27 +401,37 @@
             },
             //导出table
             downTable() {
-                // exportExcel('#tableData', this.searchForm.oilFieldName + '年度计划运行曲线表');
-                // 导出的内容只做解析，不进行格式转换
-                let queryParams=JSON.parse(JSON.stringify(this.queryParams));
-                this.queryParams.pageSize=1000;
-                this.queryParams.page=1;
-                this.$nextTick(function () {
-                    let xlsxParam = {raw: true};
-                    let wb = XLSX.utils.table_to_book(document.querySelector("#tableData"), xlsxParam);
-                    const wbout = XLSX.write(wb, {
-                        bookType: "xlsx",
-                        bookSST: true,
-                        type: "array"
-                    });
-                    try {
-                        FileSaver.saveAs(new Blob([wbout], {type: "application/octet-stream"}), this.searchForm.oilFieldName + '年度计划运行曲线表.xlsx');
-                    } catch (e) {
-                        if (typeof console !== "undefined") console.log(e, wbout);
-                    }
-                    this.queryParams=JSON.parse(JSON.stringify(queryParams));
-                    return wbout;
-                });
+                let request = {
+                    oilFieldId: this.searchForm.selectOilField,
+                    unitType: this.searchForm.selectUnitOfProduction,
+                    beginDate: this.searchForm.selectDate[0],
+                    endDate: this.searchForm.selectDate[1],
+                    planTypeCode: this.searchForm.planTypeCode,
+                    rollForecastVersion: this.searchForm.rollForecastVersion,
+                };
+                downLoadOilProductionTable(request).then(res=>{
+                    const blob = new Blob([res], { type: "application/octet-stream" });
+                    FileSaver.saveAs(blob, this.searchForm.oilFieldName + '年度计划运行曲线表.xlsx');
+                })
+                // let queryParams=JSON.parse(JSON.stringify(this.queryParams));
+                // this.queryParams.pageSize=1000;
+                // this.queryParams.page=1;
+                // this.$nextTick(function () {
+                //     let xlsxParam = {raw: true};
+                //     let wb = XLSX.utils.table_to_book(document.querySelector("#tableData"), xlsxParam);
+                //     const wbout = XLSX.write(wb, {
+                //         bookType: "xlsx",
+                //         bookSST: true,
+                //         type: "array"
+                //     });
+                //     try {
+                //         FileSaver.saveAs(new Blob([wbout], {type: "application/octet-stream"}), this.searchForm.oilFieldName + '年度计划运行曲线表.xlsx');
+                //     } catch (e) {
+                //         if (typeof console !== "undefined") console.log(e, wbout);
+                //     }
+                //     this.queryParams=JSON.parse(JSON.stringify(queryParams));
+                //     return wbout;
+                // });
             },
         },
     };
