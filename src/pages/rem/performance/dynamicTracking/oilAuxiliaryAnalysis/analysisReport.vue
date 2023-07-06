cbeb0788c89dfba17dc9ffc781282f06efb509ea
张西澎 <2395873343@qq.com>
2023-07-06 16:14:12 +0800

    修改油藏首页跳转

--- src/pages/rem/performance/dynamicTracking/oilAuxiliaryAnalysis/analysisReport.vue
+++ src/pages/rem/performance/dynamicTracking/oilAuxiliaryAnalysis/analysisReport.vue
@@ -32,6 +32,9 @@
                     <el-date-picker v-model="currentDate" type="date" value-format="yyyy-MM-dd" :clearable="false"></el-date-picker>
                     <el-button icon="el-icon-search" type="primary" style="margin-left: 20px" @click="doSearch">搜索</el-button>
                     <el-button class="commonBtn" icon="el-icon-refresh" @click="resetting">重置</el-button>
+                    <el-button  class="commonBtn"  v-if="$route.query.page" style="position: absolute;right:2%" @click="$router.push({
+                        path:$route.query.page
+                    })">返回</el-button>
                 </div>
             </headerSearch>
             <pagePanelNew style="height: calc(100% - 100px);" class="g-w100">
@@ -302,6 +305,9 @@
                     <el-date-picker v-model="currentDate" type="date" value-format="yyyy-MM-dd" :clearable="false"></el-date-picker>
                     <el-button icon="el-icon-search" type="primary" style="margin-left: 20px" @click="doSearch">搜索</el-button>
                     <el-button class="commonBtn" icon="el-icon-refresh" @click="resetting">重置</el-button>
+                    <el-button  class="commonBtn"  v-if="$route.query.page" style="position: absolute;right:2%" @click="$router.push({
+                        path:$route.query.page
+                    })">返回</el-button>
                 </div>
             </headerSearch>
             <div class="app-container3">
