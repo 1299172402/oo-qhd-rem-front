4afa5ed164ce6f6e9ea436f104bb0c057aedf0f8
chusan <chusanlyj@qq.com>
2023-07-25 09:48:55 +0800

    问题修改

--- src/components/tools/H5Chart/index.vue
+++ src/components/tools/H5Chart/index.vue
@@ -126,6 +126,8 @@
                 if (this.loadKey) {
                     if (this.w && this.w.fView && this.w.fView.RefreshData) {
                         console.log('setSampleDate')
+                        // this.w.fCommon.Delete()
+                        // this.w.fView.View.Delete()
                         this.w.fView.RefreshData(data, true, true);
                         // this.w.fView.View.ScrollView(this.w.fView.View.HScrollValue, this.w.fView.View.VScrollValue);
                         // this.w.fView.ZoomPicture();
