85a8ca75affb2c817939e3bf823a59335bbe84ca
chusan <chusanlyj@qq.com>
2023-09-05 19:15:40 +0800

    配置文件恢复

--- src/config/host.ts
+++ src/config/host.ts
@@ -7,9 +7,9 @@ const config = {
   PROCESS_PULBIC_KEY: "MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAKsr9kM6vJHJb8KgWhY6xVaYCkLEr+QKeuYUoZ2uX3+t4lzbxdf1JeJ2jUs26jWWgkpe3W1UHxXUaapyTG9sFbECAwEAAQ==",
   LOGIN_TYPES: ["password", "corp_oauth"],
   // 右上角报警点击“查看跳转”路径
-  MESSAGE_URL: "https://amm.tjioms-dev.tjltd.cnooc/#/appCallback?rediect=reportlnformation/source",
-  appId: "f198c1a239254b0e86529a0668cf4adb",
-  ALARM_URL: "https://amm.tjioms-dev.tjltd.cnooc/#/reportPolice/source?access_token=",
+  MESSAGE_URL: "https://amm.tjioms-test.tjltd.cnooc/#/appCallback?rediect=reportlnformation/source",
+  appId: "$system$",
+  ALARM_URL: "https://amm.tjioms-test.tjltd.cnooc/#/reportPolice/source?access_token=",
   WEB_TAG_NAME: "智能油田管理系统", // 网页标签名称
   SYSTEM_NAME: "天津分公司智能油田管理系统", // 系统名称
   IS_TEST_ENVIRONMENT: false
@@ -66,7 +66,7 @@ export default {
     // 文件预览服务
     PREVIEW_FILE_API: "文件服务地址",
     // 正式环境接口地址
-    API: 'https://rem.tjioms-dev.tjltd.cnooc/prod-api/',
+    API: 'https://rem.tjioms-test.tjltd.cnooc/prod-api/',
     appId: 'f198c1a239254b0e86529a0668cf4adb',
     // 正式环境 cdn 路径
     CDN: "",
