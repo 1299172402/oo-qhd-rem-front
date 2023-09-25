12c9f489fbbd1c9d30982f0195253f66fa458a6b
chusan <chusanlyj@qq.com>
2023-09-25 23:04:48 +0800

    路由修改

--- src/router/modules/home.ts
+++ src/router/modules/home.ts
@@ -1,67 +1,67 @@
 import Layout from '@/layouts/index.vue';
 
 export default [
-  {
-    // hidden: env == "development" ? false : true,
-    hidden:true,  
-    path: '/reservoirDisplay',
-    name: 'reservoirDisplay',
-    component: Layout,
-    meta: { title: '首页', icon: 'client', single: false },
-    children: [
-      {
-        path: 'linkage',
-        name: 'linkage',
-        hidden:false,
-        component: () => import('@/pages/rem/home/linkage/index.vue'),
-        meta: { title: '注采联动',icon: 'icon' },
-      },
-      {
-        path: 'oilexhibition',
-        name: 'oilexhibition',
-        component: () => import('@/pages/rem/home/reservoirDisplay/oilexhibition.vue'),
-        meta: { title: '油藏看板',icon: 'icon' },
-      },
-        {
-            path: 'worker',
-            name: 'worker',
-            component: () => import('@/pages/rem/home/reservoirDisplay/worker.vue'),
-            meta: { title: '油藏工程师看板',icon: 'icon' },
-        },
-        {
-            path: 'director',
-            name: 'director',
-            component: () => import('@/pages/rem/home/reservoirDisplay/director.vue'),
-            meta: { title: '主任工程师看板',icon: 'icon' },
-        },
-        {
-            path: 'leader',
-            name: 'leader',
-            component: () => import('@/pages/rem/home/reservoirDisplay/leader.vue'),
-            meta: { title: '分公司领导看板',icon: 'icon' },
-        },
-      {
-        path: 'shutdownDetection',
-        name: 'shutdownDetection',
-        component: () => import('@/pages/rem/home/reservoirDisplay/shutdownDetection.vue'),
-        meta: { title: '关停井跟踪',icon: 'icon' },
-      },
-      {
-        path: 'oilEventDetail',
-        name: 'oilEventDetail',
-        hidden: true,
-        component: () => import('@/pages/rem/home/reservoirDisplay/detailPage/oilEventDetail.vue'),
-        meta: { title: '油田大事件详情',icon: 'icon' },
-      },
-        {
-            path: 'problemWellStatistics',
-            name: 'problemWellStatistics',
-            hidden: true,
-            component: () => import('@/pages/rem/home/reservoirDisplay/table/problemWellStatistics.vue'),
-            meta: { title: '问题井详细',icon: 'icon' },
-        },
+  // {
+  //   // hidden: env == "development" ? false : true,
+  //   hidden:true,  
+  //   path: '/reservoirDisplay',
+  //   name: 'reservoirDisplay',
+  //   component: Layout,
+  //   meta: { title: '首页', icon: 'client', single: false },
+  //   children: [
+  //     {
+  //       path: 'linkage',
+  //       name: 'linkage',
+  //       hidden:false,
+  //       component: () => import('@/pages/rem/home/linkage/index.vue'),
+  //       meta: { title: '注采联动',icon: 'icon' },
+  //     },
+  //     {
+  //       path: 'oilexhibition',
+  //       name: 'oilexhibition',
+  //       component: () => import('@/pages/rem/home/reservoirDisplay/oilexhibition.vue'),
+  //       meta: { title: '油藏看板',icon: 'icon' },
+  //     },
+  //       {
+  //           path: 'worker',
+  //           name: 'worker',
+  //           component: () => import('@/pages/rem/home/reservoirDisplay/worker.vue'),
+  //           meta: { title: '油藏工程师看板',icon: 'icon' },
+  //       },
+  //       {
+  //           path: 'director',
+  //           name: 'director',
+  //           component: () => import('@/pages/rem/home/reservoirDisplay/director.vue'),
+  //           meta: { title: '主任工程师看板',icon: 'icon' },
+  //       },
+  //       {
+  //           path: 'leader',
+  //           name: 'leader',
+  //           component: () => import('@/pages/rem/home/reservoirDisplay/leader.vue'),
+  //           meta: { title: '分公司领导看板',icon: 'icon' },
+  //       },
+  //     {
+  //       path: 'shutdownDetection',
+  //       name: 'shutdownDetection',
+  //       component: () => import('@/pages/rem/home/reservoirDisplay/shutdownDetection.vue'),
+  //       meta: { title: '关停井跟踪',icon: 'icon' },
+  //     },
+  //     {
+  //       path: 'oilEventDetail',
+  //       name: 'oilEventDetail',
+  //       hidden: true,
+  //       component: () => import('@/pages/rem/home/reservoirDisplay/detailPage/oilEventDetail.vue'),
+  //       meta: { title: '油田大事件详情',icon: 'icon' },
+  //     },
+  //       {
+  //           path: 'problemWellStatistics',
+  //           name: 'problemWellStatistics',
+  //           hidden: true,
+  //           component: () => import('@/pages/rem/home/reservoirDisplay/table/problemWellStatistics.vue'),
+  //           meta: { title: '问题井详细',icon: 'icon' },
+  //       },
     
-    ],
+  //   ],
     
-  },
+  // },
 ];
--- src/router/modules/reservoirManagement.ts
+++ src/router/modules/reservoirManagement.ts
@@ -1,91 +1,91 @@
 import Layout from '@/layouts/index.vue';
 const env = import.meta.env.MODE;
 export default [
-    {
-        path: '/injection',
-        name: 'injection',
-        component: Layout,
-        hidden:true,
-        meta: { title: '智能注采调配', icon: 'client', single: false },
-        children: [
-            {
-                path: 'indexHome',
-                name: 'indexHome',
-                hidden:false,
-                component: () => import('@/pages/rem/intelligence/indexHome.vue'),
-                meta: { title: '首页',icon: 'icon' },
-            },
-            {
-                path: 'ipmHome',
-                name: 'ipmHome',
-                component: () => import('@/pages/rem/home/ipmHome/index.vue'),
-                meta: { title: '智能注采调配',icon: 'icon' },
-            },
-            // 油藏管理-智能注采调配-首页详情
-            {
-                path: "intelligenceDetail",
-                component: () => import("@/pages/rem/intelligence/detail.vue"),
-                name: "intelligenceDetail",
-                hidden:true,
-                meta: {
-                    title: "详情"
-                },
-            },
-            {
-                path: 'productionSplit',
-                name: 'productionSplit',
-                component: () => import('@/pages/rem/intelligence/productionSplit/index.vue'),
-                meta: { title: '产量劈分',icon: 'icon' },
-            },
-            {
-                path: 'splitSection',
-                name: 'splitSection',
-                hidden:true,
-                component: () => import('@/pages/rem/intelligence/productionSplit/splitSection.vue'),
-                meta: { title: '劈分剖面',icon: 'icon' },
-            },
-            {
-                path: 'DividingCoefficient',
-                name: 'DividingCoefficient',
-                hidden: true,
-                component: () => import('@/pages/rem/intelligence/productionSplit/DividingCoefficient.vue'),
-                meta: { title: '劈分系数',icon: 'icon' },
-            },
-            {
-                path: 'connectivityData',
-                name: 'connectivityData',
-                component: () => import('@/pages/rem/intelligence/connectivityData/connectivityData.vue'),
-                meta: { title: '井间连通性评价',icon: 'icon' },
-            },
-            {
-                path: 'coefficientCalculates',
-                name: 'coefficientCalculates',
-                hidden:true,
-                component: () => import('@/pages/rem/intelligence/connectivityData/connectivityCoefficientCalculatesProperty.vue'),
-                meta: { title: '基础计算数据',icon: 'icon' },
-            },
-            {
-                path: 'optimization',
-                name: 'optimization',
-                component: () => import('@/pages/rem/intelligence/optimization/index.vue'),
-                meta: { title: '注采调配优化',icon: 'icon' },
-            },
-            //油藏管理-注采调配优化-查看详情
-            {
-                path: "optimizationDetail",
-                name: "optimizationDetail",
-                component: () => import("@/pages/rem/intelligence/optimization/view.vue"),
-                hidden: true,
-                meta: { title: "查看详情" },
-            },
-            {
-                path: 'optimizationResult',
-                name: 'optimizationResult',
-                hidden: true,
-                component: () => import('@/pages/rem/intelligence/optimization/result.vue'),
-                meta: {title: '方案详情', icon: 'icon'},
-            },
-        ],
+    // {
+    //     path: '/injection',
+    //     name: 'injection',
+    //     component: Layout,
+    //     hidden:true,
+    //     meta: { title: '智能注采调配', icon: 'client', single: false },
+    //     children: [
+    //         {
+    //             path: 'indexHome',
+    //             name: 'indexHome',
+    //             hidden:false,
+    //             component: () => import('@/pages/rem/intelligence/indexHome.vue'),
+    //             meta: { title: '首页',icon: 'icon' },
+    //         },
+    //         {
+    //             path: 'ipmHome',
+    //             name: 'ipmHome',
+    //             component: () => import('@/pages/rem/home/ipmHome/index.vue'),
+    //             meta: { title: '智能注采调配',icon: 'icon' },
+    //         },
+    //         // 油藏管理-智能注采调配-首页详情
+    //         {
+    //             path: "intelligenceDetail",
+    //             component: () => import("@/pages/rem/intelligence/detail.vue"),
+    //             name: "intelligenceDetail",
+    //             hidden:true,
+    //             meta: {
+    //                 title: "详情"
+    //             },
+    //         },
+    //         {
+    //             path: 'productionSplit',
+    //             name: 'productionSplit',
+    //             component: () => import('@/pages/rem/intelligence/productionSplit/index.vue'),
+    //             meta: { title: '产量劈分',icon: 'icon' },
+    //         },
+    //         {
+    //             path: 'splitSection',
+    //             name: 'splitSection',
+    //             hidden:true,
+    //             component: () => import('@/pages/rem/intelligence/productionSplit/splitSection.vue'),
+    //             meta: { title: '劈分剖面',icon: 'icon' },
+    //         },
+    //         {
+    //             path: 'DividingCoefficient',
+    //             name: 'DividingCoefficient',
+    //             hidden: true,
+    //             component: () => import('@/pages/rem/intelligence/productionSplit/DividingCoefficient.vue'),
+    //             meta: { title: '劈分系数',icon: 'icon' },
+    //         },
+    //         {
+    //             path: 'connectivityData',
+    //             name: 'connectivityData',
+    //             component: () => import('@/pages/rem/intelligence/connectivityData/connectivityData.vue'),
+    //             meta: { title: '井间连通性评价',icon: 'icon' },
+    //         },
+    //         {
+    //             path: 'coefficientCalculates',
+    //             name: 'coefficientCalculates',
+    //             hidden:true,
+    //             component: () => import('@/pages/rem/intelligence/connectivityData/connectivityCoefficientCalculatesProperty.vue'),
+    //             meta: { title: '基础计算数据',icon: 'icon' },
+    //         },
+    //         {
+    //             path: 'optimization',
+    //             name: 'optimization',
+    //             component: () => import('@/pages/rem/intelligence/optimization/index.vue'),
+    //             meta: { title: '注采调配优化',icon: 'icon' },
+    //         },
+    //         //油藏管理-注采调配优化-查看详情
+    //         {
+    //             path: "optimizationDetail",
+    //             name: "optimizationDetail",
+    //             component: () => import("@/pages/rem/intelligence/optimization/view.vue"),
+    //             hidden: true,
+    //             meta: { title: "查看详情" },
+    //         },
+    //         {
+    //             path: 'optimizationResult',
+    //             name: 'optimizationResult',
+    //             hidden: true,
+    //             component: () => import('@/pages/rem/intelligence/optimization/result.vue'),
+    //             meta: {title: '方案详情', icon: 'icon'},
+    //         },
+    //     ],
 
-    },
+    // },
 ];
