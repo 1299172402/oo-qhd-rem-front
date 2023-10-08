1a44728c2331404932a27964584ca10fb40d4ed1
张西澎 <2395873343@qq.com>
2023-10-08 17:41:04 +0800

    修改代码

--- src/pages/rem/home/ipmHome/index.vue
+++ src/pages/rem/home/ipmHome/index.vue
@@ -126,7 +126,7 @@ export default {
                     },
                         {
                             name: '平面剩余油分布',
-                            url: `https://rem.${this.baseUrl}/#/injection/optimization?link=rem`
+                            url: `https://rem.${this.baseUrl}/#/injection/optimization?link=rem&page=IpmHome`
                         }],
                     boxStyle: {
                         pWidth: 'width:11vw;margin-left: 4.5vw;'
