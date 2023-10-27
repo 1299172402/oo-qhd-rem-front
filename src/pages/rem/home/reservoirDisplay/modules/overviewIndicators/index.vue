efa268e7243d62c9679f6935f7fa00ec19cf86f2
jgl <2579053233@qq.com>
2023-10-27 09:37:46 +0800

    1. 油藏看板-10⁴m³改为4为小数 2. 措施管理-天改为d

--- src/pages/rem/home/reservoirDisplay/modules/overviewIndicators/index.vue
+++ src/pages/rem/home/reservoirDisplay/modules/overviewIndicators/index.vue
@@ -24,7 +24,7 @@
                     <div class="grid-content bg-purple">
                         <div class="yield gas">
                             <div class="box">
-                                <div>{{oil1two}}</div>
+                                <div>{{Number(oil1two).toFixed(4)}}</div>
                                 <div>(10⁴m³)</div>
                             </div>
                         </div>
@@ -35,7 +35,7 @@
                     <div class="grid-content bg-purple">
                         <div class="yield oil">
                             <div class="box">
-                                <div>{{ dataList.cumeOilProd }}</div>
+                                <div>{{ Number(dataList.cumeOilProd).toFixed(4) }}</div>
                                 <div>(10⁴m³)</div>
                             </div>
                         </div>
