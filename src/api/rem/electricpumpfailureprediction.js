315690384b9a8368e675f98d8a351fa0e7dac121
jgl <2579053233@qq.com>
2023-11-16 17:05:41 +0800

    邮件正文显示图片

--- src/api/rem/electricpumpfailureprediction.js
+++ src/api/rem/electricpumpfailureprediction.js
@@ -1,7 +1,10 @@
 import request from '@/utils/request'
+
+const baseUrl = process.env.NODE_ENV == "production" ? "/rem/api" : "/rem/api"
+
 export function send(data) {
     return request({
-        url: `/message-service/message/mail/send/ipmEmail`,
+        url: `${baseUrl}/message/sendMailIncludePicture`,
         method: "post",
         data
     });
