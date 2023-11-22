12892de4b974c39359387a5908df491c901f4e95
chusan <chusanlyj@qq.com>
2023-11-22 16:43:06 +0800

    问题修改

--- src/pages/rem/performance/dynamicTracking/blockAnalysisAided/modules/fluidData/viscosityOfCrudeOil.vue
+++ src/pages/rem/performance/dynamicTracking/blockAnalysisAided/modules/fluidData/viscosityOfCrudeOil.vue
@@ -1,6 +1,7 @@
 <!--原油粘度-->
 <template>
   <page-panel-new style="height: calc(100% - 101px); margin-top: 0" show-btn>
+    <!-- :default-sort="{ prop: 'date', order: 'descending' }" -->
     <el-table
       id="tableData"
       :data="tableData"
@@ -10,13 +11,12 @@
       :cell-style="{ padding: '6px', 'text-align': 'center' }"
       style="width: 100%"
       height="100%"
-      :default-sort="{ prop: 'date', order: 'descending' }"
       :header-cell-style="{ 'text-align': 'center', padding: '0px 0' }"
     >
       <el-table-column label="序号" align="center" type="index" width="80"></el-table-column>
       <el-table-column prop="borepipeNo" label="井号" align="center" width="180"></el-table-column>
       <el-table-column prop="date" :label="`日期\n(yyyy-mm-dd)`" align="center" min-width="140"></el-table-column>
-      <el-table-column prop="sampleId" label="流体样品类型" align="center" min-width="100"></el-table-column>
+      <el-table-column prop="oilAnalyResultId" label="流体样品类型" align="center" min-width="100"></el-table-column>
       <el-table-column prop="den" :label="`原油密度\n (g/cm³)`" align="center" width="140"></el-table-column>
       <el-table-column prop="" :label="`粘度\n (mPa.s)`" align="center" width="140"></el-table-column>
       <el-table-column prop="api60F" label="API_60F" align="center" width="100"></el-table-column>
