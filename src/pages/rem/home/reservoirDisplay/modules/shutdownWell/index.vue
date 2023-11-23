d847e2f67aefd160cd099783e5553c37ce008128
jgl <2579053233@qq.com>
2023-11-23 10:49:05 +0800

    油藏看板-关停井统计-单位改为10⁴m³

--- src/pages/rem/home/reservoirDisplay/modules/shutdownWell/index.vue
+++ src/pages/rem/home/reservoirDisplay/modules/shutdownWell/index.vue
@@ -92,7 +92,7 @@ export default {
         yAxis: [
           {
             type: "value",
-            name: "关停影响产量(m³)",
+            name: "关停影响产量(10⁴m³)",
             nameTextStyle: {
               color: "#a9a8a8",
             },
