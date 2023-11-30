2f23ed3484c3b7b960e43f1b4de6b201b76ecfe7
CNOOC/ex_xulei2 <ex_xulei2@cnooc.com.cn>
2023-11-30 10:38:49 +0800

    修改默认显示326油田

--- src/pages/rem/intelligence/optimization/view.vue
+++ src/pages/rem/intelligence/optimization/view.vue
@@ -349,20 +349,31 @@ export default {
             this.queryData.dateTime = params.dateTime
         }
         this.queryWellAvgFluidProdAlloc();
-        this.getOilFields();
+        this.getuserListByUserNamesData();
     },
     methods: {
         getOilFields() {
-        let _this = this;
-        QueryOgfDetail({}).then((res) => {
-            _this.oilField = res.data.data;
-            //选择油田默认选秦皇岛32-6油田
-            if (_this.oilField.length == 0) {
-            _this.selectOilField = "";
-            } else {
-            _this.selectOilField = "3FC9A818F5BC43B88270DB80BBB3018F";
+            getFieldListsDetail({orgId:this.queryData.orgId}).then((res) => {
+                this.oilField = res.data.data;
+                var list =res.data.data;
+                for(var i=0;i<list.length;i++){
+                    if(list[i].ogfId==='3FC9A818F5BC43B88270DB80BBB3018F'){
+                        this.selectOilField=list[i].ogfId
+
+                    }
+                }
+                this.selectblock()
+            });
+        },
+        getuserListByUserNamesData(){
+            let params = {
+                searchKeys:[this.$store.getters["user/userDetail"].user.userName],
             }
-        });
+            getuserListByUserNames(params).then((res)=>{
+                this.queryData.orgId=res.data.data[0].currentTenantBindOrgId
+                this.getOilFields()
+            })
+
         },
          // 获取油田下拉数据
         selectData() {
@@ -372,12 +383,16 @@ export default {
         },
         selectblock() {
             // if (!this.selectField) return;
-            getblock({
-                ogfId: this.queryData.ogfId
-            }).then(({blockList}) => {
-                this.blockList = blockList;
-            });
+            // getblock({
+            //     ogfId: this.queryData.ogfId
+            // }).then(({blockList}) => {
+            //     this.blockList = blockList;
+            // });
             //   }
+            this.queryData.ogfId=this.selectOilField
+            getblockData({ogfId:this.queryData.ogfId}).then((res) => {
+                this.blockList = res.data.blockList;
+            });
         },
         changeOilfield() {
             this.selectblock();
