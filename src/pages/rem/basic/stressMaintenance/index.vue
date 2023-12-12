5fa1c906fb2a4f7c34be5ba990fb7ee581b94b93
chusan <chusanlyj@qq.com>
2023-12-12 17:09:58 +0800

    问题修改

--- src/pages/rem/basic/stressMaintenance/index.vue
+++ src/pages/rem/basic/stressMaintenance/index.vue
@@ -2,20 +2,11 @@
 <template>
   <div style="width: 100%; height: 100%" class="pageBox">
     <div style="display: flex; flex-direction: row; height: 100%">
-      <div style="height: 100%">
+      <!-- <div style="height: 100%">
         <tree-multiple-selection :level="'5'" @childinfo="childinfo" />
-      </div>
-      <div
-        style="
-          display: flex;
-          flex-direction: column;
-          height: calc(100%);
-          margin-left: 15px;
-          flex: 1;
-          right: 0;
-          overflow: hidden;
-        "
-      >
+      </div> 
+      margin-left: 15px;-->
+      <div style="display: flex; flex-direction: column; height: calc(100%); flex: 1; right: 0; overflow: hidden">
         <header-search class="g-w100 g-h100" style="height: auto">
           <div class="g-row-flex-V g-w100 g-h100">
             <div>
