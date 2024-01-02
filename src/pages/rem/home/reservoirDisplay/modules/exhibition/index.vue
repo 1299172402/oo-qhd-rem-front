7b4770158f05e20be7a0db0e95ef4990364b3866
张西澎 <2395873343@qq.com>
2024-01-02 10:27:44 +0800

    修改代码

--- src/pages/rem/home/reservoirDisplay/modules/exhibition/index.vue
+++ src/pages/rem/home/reservoirDisplay/modules/exhibition/index.vue
@@ -159,9 +159,7 @@ export default {
       getData(){
           let params = {
               blockId: '83D33B89B0DAB7DFA440BD060746883A',
-              yearMonth: new Date().getFullYear() + '-' + (new Date().getMonth() + 1),
-              //修改取数的日期为5月
-              // yearMonth: '2023-05',
+              yearMonth: new Date().format('yyyy-MM'),
           }
           getResidueOilCondotion(params).then(res => {
               try {
