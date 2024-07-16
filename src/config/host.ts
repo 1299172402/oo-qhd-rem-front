5649271d83f2f554dfd64e60b334b69ce0bd8557
liman7 <50509bda0aa340699586bb32f41e398b@example.com>
2024-07-16 09:04:28 +0800

    更新 host.ts

--- src/config/host.ts
+++ src/config/host.ts
@@ -1,74 +1,77 @@
 // 存放不同环境相同配置
 const config = {
-    // / 时默认跳转的路由
-    // DEFAULT_TO: "/index",
-    PROCESS_TENANT_TYPE: "tenant",
-    // rsa公钥
-    PROCESS_PULBIC_KEY: "MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAKsr9kM6vJHJb8KgWhY6xVaYCkLEr+QKeuYUoZ2uX3+t4lzbxdf1JeJ2jUs26jWWgkpe3W1UHxXUaapyTG9sFbECAwEAAQ==",
-    LOGIN_TYPES: ["password", "corp_oauth"],
-    // 右上角报警点击“查看跳转”路径
-    MESSAGE_URL: "https://amm.tjioms-dev.tjltd.cnooc/#/appCallback?rediect=reportlnformation/source",
-    // 右上角报警webSocket路径，使用时需拼接用户标示符，userId或userName均可
-    WEB_SOCKET_URL: "wss://amm.tjioms-dev.tjltd.cnooc/wss/gem001b/webSocket/",
-    appId: "f198c1a239254b0e86529a0668cf4adb",
-    ALARM_URL: "https://amm.tjioms-dev.tjltd.cnooc/#/reportPolice/source?access_token=",
-    WEB_TAG_NAME: "智能油田管理系统", // 网页标签名称
-    SYSTEM_NAME: "天津分公司智能油田管理系统", // 系统名称
-    IS_TEST_ENVIRONMENT: false
+  // / 时默认跳转的路由
+  // DEFAULT_TO: "/index",
+  PROCESS_TENANT_TYPE: "tenant",
+  // rsa公钥
+  PROCESS_PULBIC_KEY: "MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAKsr9kM6vJHJb8KgWhY6xVaYCkLEr+QKeuYUoZ2uX3+t4lzbxdf1JeJ2jUs26jWWgkpe3W1UHxXUaapyTG9sFbECAwEAAQ==",
+  LOGIN_TYPES: ["password", "corp_oauth"],
+  // 右上角报警点击“查看跳转”路径
+  MESSAGE_URL: "https://amm.tjioms-test.tjltd.cnooc/#/appCallback?rediect=reportlnformation/source",
+  // 右上角报警webSocket路径，使用时需拼接用户标示符，userId或userName均可
+  WEB_SOCKET_URL: "wss://amm.tjioms-test.tjltd.cnooc/wss/gem001b/webSocket/",
+  appId: "$system$",
+  ALARM_URL: "https://amm.tjioms-test.tjltd.cnooc/#/reportPolice/source?access_token=",
+  WEB_TAG_NAME: "智能油田管理系统", // 网页标签名称
+  SYSTEM_NAME: "天津分公司智能油田管理系统", // 系统名称
+  IS_TEST_ENVIRONMENT: false
 };
 
 export default {
-    development: {
-        ...config,
-        // TODO: Maybe change back
-        // 通用模块-报警中心
-        // GEM_API: "/gem001b",
-        // 文件预览服务
-        PREVIEW_FILE_API: "/b/upload",
-        appId: 'f198c1a239254b0e86529a0668cf4adb',
-        // 流程平台
-        processAPI: "/dev-api/workflow",
-        // 开发环境接口请求
-        API: "/dev-api",
-        // API: '',
-        // 开发环境 cdn 路径
-        CDN: ""
-    },
-    test: {
-        ...config,
-        // 通用模块-报警中心
-        // TODO: Maybe change back
-        // GEM_API: "/gem001b",
-        // 流程平台
-        // 文件预览服务
-        PREVIEW_FILE_API: "文件服务地址",
-        // 测试环境接口地址
-        API: "",
-        // 测试环境 cdn 路径
-        CDN: ""
-    },
-    stage: {
-        ...config,
-        processAPI: "",
-        // 正式环境接口地址
-        API: "",
-        // 正式环境 cdn 路径
-        CDN: ""
-    },
-    release: {
-        ...config,
-        // 通用模块-报警中心
-        // TODO: Maybe change back
-        // GEM_API: "/gem001b",
-        // 流程平台
-        processAPI: "/prod-api/workflow",
-        // 文件预览服务
-        PREVIEW_FILE_API: "文件服务地址",
-        API: 'https://rem.tjioms-dev.tjltd.cnooc/prod-api/',
-        appId: 'f198c1a239254b0e86529a0668cf4adb',
-        // 正式环境接口地址
-        // 正式环境 cdn 路径
-        CDN: "",
-        LOGIN_TYPES: ["corp_oauth", "password"]
-    }
+  development: {
+    ...config,
+    // TODO: Maybe change back
+    // 通用模块-报警中心
+    // GEM_API: "/gem001b",
+    // 流程平台
+    BIZ_FLOW_API: "/biz/flow",
+    // 文件预览服务
+    PREVIEW_FILE_API: "/b/upload",
+    processAPI: "/dev-api/workflow",
+    appId: 'f198c1a239254b0e86529a0668cf4adb',
+    // 开发环境接口请求
+    API: "/dev-api",
+    // API: '',
+    // 开发环境 cdn 路径
+    CDN: ""
+  },
+  test: {
+    ...config,
+    // TODO: Maybe change back
+    // 通用模块-报警中心
+    // GEM_API: "/gem001b",
+    // 流程平台
+    BIZ_FLOW_API: "/biz/flow",
+    // 文件预览服务
+    PREVIEW_FILE_API: "文件服务地址",
+    // 测试环境接口地址
+    API: "",
+    // 测试环境 cdn 路径
+    CDN: ""
+  },
+  stage: {
+    ...config,
+    processAPI: "",
+    // 正式环境接口地址
+    API: "",
+    // 正式环境 cdn 路径
+    CDN: ""
+  },
+  release: {
+    ...config,
+    // 通用模块-报警中心
+    // TODO: Maybe change back
+    // GEM_API: "/gem001b",
+    // 流程平台
+    BIZ_FLOW_API: "/b/sw",
+    processAPI: "/prod-api/workflow",
+    // 文件预览服务
+    PREVIEW_FILE_API: "文件服务地址",
+    // 正式环境接口地址
+    API: 'https://rem.tjioms-test.tjltd.cnooc/prod-api/',
+    appId: 'f198c1a239254b0e86529a0668cf4adb',
+    // 正式环境 cdn 路径
+    CDN: "",
+    LOGIN_TYPES: ["corp_oauth", "password"]
+  }
 };
