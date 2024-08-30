cbe54231ed06c8ea20089d76f9d275c16ec714e0
29673994 <83c776dbc31077b2a0cac744489d7a64@example.com>
2024-08-30 16:13:32 +0800

    添加路由权限

--- src/utils/routerMapping/routerPermission.js
+++ src/utils/routerMapping/routerPermission.js
@@ -121,7 +121,9 @@ const GenerateRouter = {
   "HorizontalMaintenance": () => import("@/pages/rem/basic/horizontalMaintenance/index.vue"),
   "StressMaintenance": () => import("@/pages/rem/basic/stressMaintenance/index.vue"),
 
-    
+  "FlowModel": () => import("@/pages/horizontalWell/flowmodel"),
+  "Forewarning": () => import("@/pages/horizontalWell/forewarning"),
+  "CompartmentDesign": () => import("@/pages/horizontalWell/compartmentdesign"),
     
   // 
   // "": () => import(""),
